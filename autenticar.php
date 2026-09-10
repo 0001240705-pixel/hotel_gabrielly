238dc842a672d46ef73ec6d46e4fa7c89c46f411
Gabrielly <0001240705@senaimgaluno.com.br>
2026-09-09 22:08:49 -0300

    versão 1.0

--- autenticar.php
+++ autenticar.php
@@ -6,29 +6,15 @@ include("conexao.php");
 $email = $_POST["email"];
 $senha = $_POST["senha"];
 
-$sql = "SELECT * FROM clientes WHERE email = ?";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("s", $email);
-$stmt->execute();
+$sql = "SELECT * FROM clientes WHERE email = '$email' and senha = '$senha' ";
+$resultado = mysqli_query($conexao, $sql);
 
-$resultado = $stmt->get_result();
-
-if ($resultado->num_rows > 0) {
-    $cliente = $resultado->fetch_assoc();
-
-    // Verifica a senha criptografada
-    if (password_verify($senha, $cliente["senha"])) {
-
-        $_SESSION["usuario"] = $cliente["email"];
-
-        header("Location: minhas_reservas.php");
-        exit();
-
-    } else {
-        echo "Senha incorreta! <a href='login.html'>Voltar ao login</a>";
-    }
-
-} else {
-    echo "E-mail incorreto! <a href='login.html'>Voltar ao login</a>";
+if (mysqli_num_rowa ($resultado) > 0) {
+    header ("Location: minhas_reservas.php");
+    exit ();
 }
+ else {
+    header ("Location: login.html.html");
+    exit ();
+ }
 ?>
